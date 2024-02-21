53b24393f2e6d84114c70250d43c7df5e3523812
Richard Hess <eswat2@gmail.com>
2024-02-20 17:27:17 -0800

    » updating packages

--- src/lit-tinker-app.js
+++ src/lit-tinker-app.js
@@ -7,27 +7,16 @@ import {
   valuesFC,
 } from "./components"
 import appStyles from "./styles/app.css?raw"
+import { StoreController } from "@nanostores/lit"
 import { actions, state } from "./utils"
 
 export class LitTinkerApp extends LitElement {
+  sc = new StoreController(this, state)
+
   constructor() {
     super()
 
     actions.init()
-
-    const { clicks, pick, sample } = state.get()
-
-    this.clicks = clicks
-    this.pick = pick
-    this.sample = sample
-
-    state.listen((value, key) => {
-      if (this[key] != value[key]) {
-        this[key] = value[key]
-
-        this.requestUpdate()
-      }
-    })
   }
 
   render() {
@@ -35,10 +24,14 @@ export class LitTinkerApp extends LitElement {
 
     return html`
       <div class="ds1-main">
-        ${eswat2FC()} ${headerFC()} ${controlsFC({ clicks: this.clicks })}
-        ${colorsFC({ pick: this.pick })}
+        ${eswat2FC()} ${headerFC()}
+        ${controlsFC({ clicks: this.sc.value.clicks })}
+        ${colorsFC({ pick: this.sc.value.pick })}
         <hr class="ml-0 mr-0" />
-        ${valuesFC({ pick: this.pick, sample: this.sample })}
+        ${valuesFC({
+          pick: this.sc.value.pick,
+          sample: this.sc.value.sample,
+        })}
       </div>
     `
   }
